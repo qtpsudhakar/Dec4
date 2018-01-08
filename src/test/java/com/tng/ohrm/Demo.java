c8d284117658de048f3faed253409c4701ec1c49
qtpsudhakar <sudhakar@qtpsudhakar>
2018-01-08 08:23:05 +0530

    new change 2

--- src/test/java/com/tng/ohrm/Demo.java
+++ src/test/java/com/tng/ohrm/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is new");
+		System.out.println("A new change");
 	}
 
 }
